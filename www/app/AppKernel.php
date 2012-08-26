52783b55e1109f6c763395becda758602870b2c8
sergey belov <limitium@gmail.com>
2012-08-27 01:31:14 +0400

    fosuser

--- www/app/AppKernel.php
+++ www/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Mera\PostBundle\MeraPostBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
