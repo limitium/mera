1f97d441ffda040cc37f3f7d632b0bb4ab4ca9a5
Sergey Belov <limitium@gmail.com>
2012-09-13 16:25:15 +0400

    collect item adding

--- www/web/js/app.js
+++ www/web/js/app.js
@@ -6,4 +6,15 @@ $(document).ready(function () {
         but.closest(".accordion-group").next().children(".collapse").collapse("show");
         return false;
     })
+    $(".add-collection-item").click(function () {
+        var button = $(this),
+            collection = button.closest("[data-prototype]"),
+            prototype = collection.data("prototype"),
+            collectionContainer = $(".collection-rows", collection),
+            rowNumber = $(".collection-rows").children().length,
+            newRow = prototype.split("__name__").join(rowNumber);
+
+        collectionContainer.append(newRow);
+        return false;
+    });
 });
