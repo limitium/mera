e1e0c4321d8247cf28f4c98ebc94758313da5cc8
Sergey Belov <limitium@gmail.com>
2012-09-28 18:03:33 +0400

    upload works

--- www/src/Mera/AuditBundle/Controller/AuditController.php
+++ www/src/Mera/AuditBundle/Controller/AuditController.php
@@ -8,8 +8,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Mera\AuditBundle\Entity\Building;
-use Mera\AuditBundle\Entity\ConstructElement;
+use Mera\AuditBundle\Classes\UploadHandler;
 use Mera\AuditBundle\Form\CommonType;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Audit controller.
@@ -93,6 +94,67 @@ class AuditController extends Controller
         $em->flush();
 
         return $this->redirect($this->generateUrl('audit'));
+    }
+
+    /**
+     * Save a Audit audit.
+     *
+     * @Route("/file", name="audit_file")
+     * @Method("POST")
+     */
+    public function fileAction(Request $request)
+    {
+//        $em = $this->getDoctrine()->getEntityManager();
+//
+//        if(!$projet = $em->getRepository('BoldPrezBundle:Projet')->find($id)) {
+//            throw $this->createNotFoundException('Le projet[id='.$id.'] n\'existe pas.');
+//        }
+//        $client = $projet->getClient();
+//
+        $request = $this->get('request');
+
+        $options = array(
+            'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']) . '/img/scans/',
+            'image_versions' => array(
+                'thumbnail' => array(
+                    'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']) . '/img/thumbnails/',
+                    'max_width' => 80,
+                    'max_height' => 80
+                )
+            )
+        );
+        $upload_handler = new UploadHandler($options, "");
+
+
+        switch ($request->getMethod()) {
+            case 'OPTIONS':
+                break;
+            case 'HEAD':
+            case 'GET':
+                $upload_handler->get();
+                break;
+            case 'POST':
+                if ($request->get('_method') === 'DELETE') {
+                    $upload_handler->delete();
+                } else {
+                    $upload_handler->post();
+                }
+                break;
+            case 'DELETE':
+                $upload_handler->delete();
+                break;
+            default:
+                header('HTTP/1.1 405 Method Not Allowed');
+        }
+
+        $response = new Response();
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate');
+        $response->headers->set('Content-Disposition', 'inline; filename="files.json"');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, POST, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-File-Name, X-File-Type, X-File-Size');
+        return $response;
     }
 
     private function getCommon()
